b8133d3dd9366cf41689f158ed33e7ec01d8bfb0
Chris Lime <chrkalivas@gmail.com>
2023-12-13 00:13:23 +0200

    lintin yo

--- packages/ui/src/button.tsx
+++ packages/ui/src/button.tsx
@@ -11,8 +11,8 @@ interface ButtonProps {
 export function Button({children, className, appName}: ButtonProps) {
   return (
     <button
-      className={className}
       onClick={() => alert(`Hello from your ${appName} app!`)}
+      className={className}
       type="button">
       {children}
     </button>
