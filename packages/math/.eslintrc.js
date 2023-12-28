811a08d78e28777eb2794619d849173ecd3b6f69
Chris Lime <chrkalivas@gmail.com>
2023-12-28 12:19:15 +0200

    Add ESLint configuration and TypeScript files for math package

---
+++ packages/math/.eslintrc.js
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	root: true,
+	extends: [require.resolve('@repo/lint/node.js')],
+	parserOptions: {
+		project: true,
+	},
+	plugins: ['@typescript-eslint'],
+}
