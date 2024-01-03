9295e021953b88967b5249d42696005bb50c6c09
Chris Lime <chrkalivas@gmail.com>
2024-01-03 19:50:21 +0200

    Update eslint configuration and add new component

--- packages/yolo-ui/.eslintrc.cjs
+++ packages/yolo-ui/.eslintrc.cjs
@@ -1,18 +1,28 @@
+/** @type {import("eslint").Linter.Config} */
 module.exports = {
-  root: true,
-  env: { browser: true, es2020: true },
-  extends: [
-    'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
-  ],
-  ignorePatterns: ['dist', '.eslintrc.cjs'],
-  parser: '@typescript-eslint/parser',
-  plugins: ['react-refresh'],
-  rules: {
-    'react-refresh/only-export-components': [
-      'warn',
-      { allowConstantExport: true },
-    ],
-  },
+	root: true,
+	extends: [require.resolve('@repo/lint/vite.js')],
+	parserOptions: {
+		project: [`${__dirname}/tsconfig.json`, `${__dirname}/tsconfig.node.json`],
+	},
+	// plugins: ['@typescript-eslint'],
 }
+
+// /** @type {import("eslint").Linter.Config} */
+// module.exports = {
+// 	root: true,
+// 	extends: [require.resolve('@repo/lint/react-internal.js')],
+// 	parserOptions: {
+// 		project: true,
+// 	},
+// }
+
+// /** @type {import("eslint").Linter.Config} */
+// module.exports = {
+// 	root: true,
+// 	extends: [require.resolve('@repo/lint/react-internal.js')],
+// 	parserOptions: {
+// 		project: [`${__dirname}/tsconfig.json`, `${__dirname}/tsconfig.node.json`],
+// 	},
+// 	// plugins: ['@typescript-eslint'],
+// }
--- packages/yolo-ui/src/application.tsx
+++ packages/yolo-ui/src/application.tsx
@@ -1,9 +1,21 @@
-function App() {
+export function App() {
 	return (
 		<>
-			<h1>This is my Yolo UI Yall</h1>
+			<h1 className="hello">This is my Yolo UI Yall</h1>
+			<Lorem />
 		</>
 	)
 }
 
-export default App
+export function Lorem() {
+	return (
+		<div className="yo">
+			<p>
+				Lorem ipsum dolor sit amet consectetur adipisicing elit. Sapiente error
+				veniam provident perferendis at, magni impedit ut. Reiciendis nobis, non
+				praesentium at est incidunt, corrupti nisi molestiae modi nulla
+				sapiente.
+			</p>
+		</div>
+	)
+}
