8396fbffdc223c8f8746781f81c1472c1dd8c798
Chris Lime <chrkalivas@gmail.com>
2024-01-04 12:33:20 +0200

    Rename yolui

--- packages/yolui/vite.config.ts
+++ packages/yolui/vite.config.ts
@@ -1,7 +1,15 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
+import {resolve} from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	plugins: [react()],
+	build: {
+		lib: {
+			entry: resolve(__dirname, 'src/components/index.ts'),
+			name: 'yolo-ui',
+			fileName: 'yolui',
+		},
+	},
 })
