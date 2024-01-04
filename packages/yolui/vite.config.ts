e6f754e40aa97d06c306b9e2bbda6e90dc8d2fa8
Chris Lime <chrkalivas@gmail.com>
2024-01-04 14:46:32 +0200

    Update yolui package.json and vite.config.ts for build

--- packages/yolui/vite.config.ts
+++ packages/yolui/vite.config.ts
@@ -1,15 +1,34 @@
 import {defineConfig} from 'vite'
 import react from '@vitejs/plugin-react'
 import {resolve} from 'path'
+import dts from 'vite-plugin-dts'
+import {libInjectCss} from 'vite-plugin-lib-inject-css'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [react()],
+	plugins: [
+		react(),
+		libInjectCss(),
+		dts({
+			include: ['src/components'],
+		}),
+	],
 	build: {
+		copyPublicDir: false,
 		lib: {
 			entry: resolve(__dirname, 'src/components/index.ts'),
 			name: 'yolo-ui',
 			fileName: 'yolui',
+		},
+		rollupOptions: {
+			external: ['react', 'react-dom', 'react/jsx-runtime'],
+			output: {
+				globals: {
+					react: 'React',
+					'react-dom': 'ReactDOM',
+					'react/jsx-runtime': 'jsxRuntime',
+				},
+			},
 		},
 	},
 })
