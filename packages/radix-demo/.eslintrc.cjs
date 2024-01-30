91026309672855f4f1a89b99bcac3b465b046595
Chris Lime <chrkalivas@gmail.com>
2024-01-31 00:57:14 +0200

    (radix-demo): Update ESLint configuration and add PostCSS and TailwindCSS

--- packages/radix-demo/.eslintrc.cjs
+++ packages/radix-demo/.eslintrc.cjs
@@ -2,6 +2,7 @@
 module.exports = {
 	root: true,
 	extends: [require.resolve('@repo/lint/vite.js')],
+	ignorePatterns: ['postcss.config.js', 'tailwind.config.js'],
 	parserOptions: {
 		project: [`${__dirname}/tsconfig.json`, `${__dirname}/tsconfig.node.json`],
 	},
