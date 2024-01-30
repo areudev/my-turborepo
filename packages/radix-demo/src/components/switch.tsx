0f905a30c72d632b9edd253fb7f7af591318ac65
Chris Lime <chrkalivas@gmail.com>
2024-01-31 01:23:33 +0200

    Update Switch component styles

--- packages/radix-demo/src/components/switch.tsx
+++ packages/radix-demo/src/components/switch.tsx
@@ -2,8 +2,8 @@ import * as Switch from '@radix-ui/react-switch'
 
 export function SwitchDemo() {
 	return (
-		<Switch.Root className="h-5 w-5 rounded-full bg-gray-200 data-[state=checked]:bg-green-500">
-			<Switch.Thumb />
+		<Switch.Root className="w-11 rounded-full bg-gray-700 p-px shadow-inner shadow-black/50 transition duration-700 active:bg-gray-600  data-[state=checked]:bg-sky-500 active:data-[state=checked]:bg-sky-400">
+			<Switch.Thumb className="block h-6 w-6 rounded-full bg-gray-200 shadow-sm transition duration-700 data-[state=checked]:translate-x-[18px] data-[state=checked]:bg-white" />
 		</Switch.Root>
 	)
 }
