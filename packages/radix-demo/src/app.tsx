c804bfafebe06aaf2d3266f125424ee1c4a1ebc8
Chris Lime <chrkalivas@gmail.com>
2024-01-31 01:00:11 +0200

    (radix-demo): Add @radix-ui/react-switch dependency and import SwitchDemo component

--- packages/radix-demo/src/app.tsx
+++ packages/radix-demo/src/app.tsx
@@ -1,11 +1,9 @@
+import {SwitchDemo} from './components/switch'
+
 export function App() {
 	return (
 		<div>
-			<div>
-				<h1 className="text-5xl">Radix Demo</h1>
-				<h2>Hello</h2>
-				<h3>What</h3>
-			</div>
+			<SwitchDemo />
 		</div>
 	)
 }
---
+++ packages/radix-demo/src/components/switch.tsx
@@ -0,0 +1,9 @@
+import * as Switch from '@radix-ui/react-switch'
+
+export function SwitchDemo() {
+	return (
+		<Switch.Root>
+			<Switch.Thumb />
+		</Switch.Root>
+	)
+}
