619b709f1200a6355c7c213fb5b71fb171795f98
Chris Lime <chrkalivas@gmail.com>
2024-01-31 00:46:59 +0200

    Add Vite configuration and setup React app

---
+++ packages/radix-demo/.eslintrc.cjs
@@ -0,0 +1,9 @@
+/** @type {import("eslint").Linter.Config} */
+module.exports = {
+	root: true,
+	extends: [require.resolve('@repo/lint/vite.js')],
+	parserOptions: {
+		project: [`${__dirname}/tsconfig.json`, `${__dirname}/tsconfig.node.json`],
+	},
+	// plugins: ['@typescript-eslint'],
+}
---
+++ packages/radix-demo/src/app.tsx
@@ -0,0 +1,11 @@
+export function App() {
+	return (
+		<div>
+			<div>
+				<h1>Radix Demo</h1>
+				<h2>Hello</h2>
+				<h3>What</h3>
+			</div>
+		</div>
+	)
+}
