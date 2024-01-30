db84337569b01f35b96be20121a390d978efd8b0
Chris Lime <chrkalivas@gmail.com>
2024-01-31 01:38:49 +0200

    Refactor switch component to use label and add switchId

--- packages/radix-demo/src/components/switch.tsx
+++ packages/radix-demo/src/components/switch.tsx
@@ -1,9 +1,17 @@
 import * as Switch from '@radix-ui/react-switch'
+import { useId } from 'react'
 
 export function SwitchDemo() {
+	const switchId = useId()
 	return (
-		<Switch.Root className="w-11 rounded-full bg-gray-700 p-px shadow-inner shadow-black/50 transition duration-700 active:bg-gray-600  data-[state=checked]:bg-sky-500 active:data-[state=checked]:bg-sky-400">
-			<Switch.Thumb className="block h-6 w-6 rounded-full bg-gray-200 shadow-sm transition duration-700 data-[state=checked]:translate-x-[18px] data-[state=checked]:bg-white" />
-		</Switch.Root>
+		<label className="flex space-x-4" htmlFor={switchId}>
+			<span className="font-medium">Airplane mode</span>
+			<Switch.Root
+				className="w-11 rounded-full bg-gray-700 p-px shadow-inner shadow-black/50 transition focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-400 active:bg-gray-600 data-[state=checked]:bg-sky-500 active:data-[state=checked]:bg-sky-400"
+				id={switchId}
+			>
+				<Switch.Thumb className="block h-6 w-6 rounded-full bg-gray-200 shadow-sm transition  data-[state=checked]:translate-x-[18px] data-[state=checked]:bg-white" />
+			</Switch.Root>
+		</label>
 	)
 }
