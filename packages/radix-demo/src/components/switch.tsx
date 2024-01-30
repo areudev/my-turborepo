d044a64beeed10916e87ef2e430301721ef42003
Chris Lime <chrkalivas@gmail.com>
2024-01-31 01:10:38 +0200

    (radix-demo): Update tailwind colors

--- packages/radix-demo/src/components/switch.tsx
+++ packages/radix-demo/src/components/switch.tsx
@@ -2,7 +2,7 @@ import * as Switch from '@radix-ui/react-switch'
 
 export function SwitchDemo() {
 	return (
-		<Switch.Root className="h-5 w-5 bg-white">
+		<Switch.Root className="h-5 w-5 rounded-full bg-gray-200 data-[state=checked]:bg-green-500">
 			<Switch.Thumb />
 		</Switch.Root>
 	)
--- packages/radix-demo/tailwind.config.js
+++ packages/radix-demo/tailwind.config.js
@@ -1,8 +1,14 @@
 /** @type {import('tailwindcss').Config} */
+
+import colors from 'tailwindcss/colors'
 export default {
 	content: ['./index.html', './src/**/*.{js,ts,jsx,tsx}'],
 	theme: {
-		extend: {},
+		extend: {
+			colors: {
+				gray: colors.zinc,
+			},
+		},
 	},
 	plugins: [],
 }
