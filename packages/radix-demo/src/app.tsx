c6eea9162cc3996e3c2b4213c02504d98f2e35e2
Chris Lime <chrkalivas@gmail.com>
2024-02-03 11:47:59 +0200

    yo

--- packages/radix-demo/src/app.tsx
+++ packages/radix-demo/src/app.tsx
@@ -1,10 +1,10 @@
-// import { SwitchDemo } from './components/switch-demo'
+import { SwitchDemo } from './components/switch-demo'
 import { SelectorDemo } from './components/selector-demo'
 
 export function App() {
 	return (
-		<div className="flex min-h-screen items-center justify-center">
-			{/* <SwitchDemo /> */}
+		<div className="flex min-h-screen flex-col items-center justify-center gap-2">
+			<SwitchDemo />
 			<SelectorDemo />
 		</div>
 	)
