930ee139e815b8bbba23e653f2a96d9309c7c828
Chris Lime <chrkalivas@gmail.com>
2024-02-06 19:53:20 +0200

    Refactor app.tsx and slider.tsx

--- packages/radix-demo/src/app.tsx
+++ packages/radix-demo/src/app.tsx
@@ -1,11 +1,14 @@
 // import { SwitchDemo } from './components/switch-demo'
-import { SelectorDemo } from './components/selector-demo'
+// import { SelectorDemo } from './components/selector-demo'
+
+import { SliderDemo } from './components/slider-demo'
 
 export function App() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center gap-2">
 			{/* <SwitchDemo /> */}
-			<SelectorDemo />
+			{/* <SelectorDemo /> */}
+			<SliderDemo />
 		</div>
 	)
 }
--- packages/radix-demo/src/lib/slider.tsx
+++ packages/radix-demo/src/lib/slider.tsx
@@ -8,11 +8,15 @@ const Slider = React.forwardRef<
 	React.ElementRef<typeof SliderPrimitive.Root>,
 	React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
 >(({ className, ...props }, ref) => (
-	<SliderPrimitive.Root className={cn('', className)} ref={ref} {...props}>
-		<SliderPrimitive.Track>
-			<SliderPrimitive.Range />
+	<SliderPrimitive.Root
+		className={cn('relative flex h-1.5 items-center', className)}
+		ref={ref}
+		{...props}
+	>
+		<SliderPrimitive.Track className="h-full grow bg-white">
+			<SliderPrimitive.Range className="absolute h-full bg-blue-500" />
 		</SliderPrimitive.Track>
-		<SliderPrimitive.Thumb />
+		<SliderPrimitive.Thumb className="block h-5 w-5 rounded-full bg-blue-400" />
 	</SliderPrimitive.Root>
 ))
 Slider.displayName = SliderPrimitive.Root.displayName
