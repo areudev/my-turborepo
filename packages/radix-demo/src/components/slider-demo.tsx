6b7eaf203aa1897000ee497e14e2e93bfd4d858b
Chris Lime <chrkalivas@gmail.com>
2024-02-06 19:47:50 +0200

    Wrong slider yooo

---
+++ packages/radix-demo/src/components/slider-demo.tsx
@@ -0,0 +1,11 @@
+import { Slider } from '../lib/slider'
+
+export function SliderDemo() {
+	return (
+		<div>
+			<h1>Slider Demo</h1>
+			<p>Coming soon...</p>
+			<Slider />
+		</div>
+	)
+}
--- packages/radix-demo/src/lib/slider.tsx
+++ packages/radix-demo/src/lib/slider.tsx
@@ -1,17 +1,20 @@
+'use client'
+
 import * as React from 'react'
-import * as ProgressPrimitive from '@radix-ui/react-progress'
+import * as SliderPrimitive from '@radix-ui/react-slider'
+import { cn } from '../utils/misc'
 
-// import { cn } from '../utils/misc'
+const Slider = React.forwardRef<
+	React.ElementRef<typeof SliderPrimitive.Root>,
+	React.ComponentPropsWithoutRef<typeof SliderPrimitive.Root>
+>(({ className, ...props }, ref) => (
+	<SliderPrimitive.Root className={cn('', className)} ref={ref} {...props}>
+		<SliderPrimitive.Track>
+			<SliderPrimitive.Range />
+		</SliderPrimitive.Track>
+		<SliderPrimitive.Thumb />
+	</SliderPrimitive.Root>
+))
+Slider.displayName = SliderPrimitive.Root.displayName
 
-export const Progress = React.forwardRef<
-	React.ElementRef<typeof ProgressPrimitive.Root>,
-	React.ComponentPropsWithoutRef<typeof ProgressPrimitive.Root>
->((props, ref) => {
-	return (
-		<ProgressPrimitive.Root ref={ref} {...props}>
-			<ProgressPrimitive.Indicator />
-		</ProgressPrimitive.Root>
-	)
-})
-
-Progress.displayName = ProgressPrimitive.Root.displayName
+export { Slider }
