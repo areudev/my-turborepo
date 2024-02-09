81eca6bcc84badde552abd9352f6fdb5d2b16a7b
Chris Lime <chrkalivas@gmail.com>
2024-02-09 20:54:17 +0200

    Refactor SliderDemo component

--- packages/radix-demo/src/components/slider-demo.tsx
+++ packages/radix-demo/src/components/slider-demo.tsx
@@ -7,7 +7,7 @@ const MIN = 0
 const MAX = 100
 const STEP = 1
 
-export function SliderDemo({
+function SliderVolume({
 	min = MIN,
 	max = MAX,
 	step = STEP,
@@ -19,10 +19,63 @@ export function SliderDemo({
 	const [usingPointer, setUsingPointer] = useState(false)
 	const [internalValue, setInternalValue] = useState(50)
 	const [stash, setStash] = useState({ clientX: 0, internalValue })
-	// const [startingClientX, setStartingClientX] = useState(0)
-	// const [startingInternalValue, setStartingInternalValue] =
-	// 	useState(internalValue)
 
+	return (
+		<>
+			<div className="*:duration-[350ms] group flex items-center gap-3 transition-[margin] duration-[350ms] hover:-mx-3 hover:cursor-grab active:cursor-grabbing">
+				<SpeakerXMark className="size-5 duration-[350ms] group-hover:scale-125" />
+				<Slider
+					rootProps={{
+						name: 'what',
+						min,
+						max,
+						step,
+						value: [internalValue],
+						onBlur: () => {
+							setUsingPointer(false)
+						},
+						onValueCommit: ([value]) => {
+							if (!usingPointer) setInternalValue(value)
+						},
+						className:
+							'grow transition-[height] duration-[350ms] group-hover:h-4',
+					}}
+					trackProps={{
+						onPointerDown: e => {
+							setUsingPointer(true)
+							setStash({ clientX: e.clientX, internalValue })
+						},
+						onPointerMove: e => {
+							if (e.buttons > 0) {
+								const diffInPixels = e.clientX - stash.clientX
+								const sliderWidth = e.currentTarget.clientWidth
+								const pixelsPerUnit = (max - min) / sliderWidth
+								const diffInUnits = diffInPixels * pixelsPerUnit
+								const newValue = stash.internalValue + diffInUnits
+								const clampedValue = Math.max(Math.min(newValue, max), min)
+								const stepppedValue = Math.round(clampedValue / step) * step
+								setInternalValue(stepppedValue)
+							}
+						},
+
+						className: cn(
+							'grow',
+							!usingPointer &&
+								'group-has-[:focus-visible]:outline group-has-[:focus-visible]:outline-2 group-has-[:focus-visible]:outline-offset-2 group-has-[:focus-visible]:outline-sky-500',
+						),
+					}}
+					rangeProps={{
+						className: 'group-has-[:focus-visible]:bg-white',
+					}}
+				/>
+				<SpeakerWave className="h-5 w-5 duration-[350ms] group-hover:scale-125" />
+			</div>
+			<p>internal value {internalValue}</p>
+		</>
+	)
+}
+
+export function SliderDemo() {
 	return (
 		<div className="mx-auto w-full max-w-xs">
 			<form
@@ -34,57 +87,10 @@ export function SliderDemo({
 					alert(json)
 				}}
 			>
-				<p>internal value {internalValue}</p>
+				{/* <p>internal value {internalValue}</p> */}
 				<p className="text-sm font-medium text-white/60">Settings</p>
-				<div className="*:duration-[350ms] group flex items-center gap-3 transition-[margin] duration-[350ms] hover:-mx-3 hover:cursor-grab active:cursor-grabbing">
-					<SpeakerXMark className="size-5 duration-[350ms] group-hover:scale-125" />
-					<Slider
-						rootProps={{
-							name: 'what',
-							min,
-							max,
-							step,
-							value: [internalValue],
-							onBlur: () => {
-								setUsingPointer(false)
-							},
-							onValueCommit: ([value]) => {
-								if (!usingPointer) setInternalValue(value)
-							},
-							className:
-								'grow transition-[height] duration-[350ms] group-hover:h-4',
-						}}
-						trackProps={{
-							onPointerDown: e => {
-								setUsingPointer(true)
-								setStash({ clientX: e.clientX, internalValue })
-							},
-							onPointerMove: e => {
-								if (e.buttons > 0) {
-									const diffInPixels = e.clientX - stash.clientX
-									const sliderWidth = e.currentTarget.clientWidth
-									const pixelsPerUnit = (max - min) / sliderWidth
-									const diffInUnits = diffInPixels * pixelsPerUnit
-									const newValue = stash.internalValue + diffInUnits
-									const clampedValue = Math.max(Math.min(newValue, max), min)
-									const stepppedValue = Math.round(clampedValue / step) * step
-									setInternalValue(stepppedValue)
-								}
-							},
-
-							className: cn(
-								'grow',
-								!usingPointer &&
-									'group-has-[:focus-visible]:outline group-has-[:focus-visible]:outline-2 group-has-[:focus-visible]:outline-offset-2 group-has-[:focus-visible]:outline-sky-500',
-							),
-						}}
-						rangeProps={{
-							className: 'group-has-[:focus-visible]:bg-white',
-						}}
-					/>
-					<SpeakerWave className="h-5 w-5 duration-[350ms] group-hover:scale-125" />
-				</div>
-				{/* <input className="w-full" name="native" type="range" /> */}
+				<SliderVolume />
+				{/* Rest of the form */}
 				<div className="mt-5 flex items-center justify-between">
 					<button
 						className="focus-visible-visible:outline-2 rounded bg-white/[.15] px-3 py-1 font-medium text-white hover:bg-white/25 focus:outline-none focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-sky-500"
