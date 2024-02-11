4f86cb1b05d3498bf57cefc8449cb9a690ec510a
Chris Lime <chrkalivas@gmail.com>
2024-02-11 23:41:35 +0200

    Add @radix-ui/react-toast package

--- packages/radix-demo/src/lib/toast.tsx
+++ packages/radix-demo/src/lib/toast.tsx
@@ -1,4 +1,27 @@
-'use client'
+import React from 'react'
+import * as ToastPrimitives from '@radix-ui/react-toast'
+import { cn } from '../utils/misc'
+
+const ToastProvider = ToastPrimitives.ToastProvider
+
+const ToastViewport = React.forwardRef<
+	React.ElementRef<typeof ToastPrimitives.ToastViewport>,
+	React.ComponentPropsWithoutRef<typeof ToastPrimitives.ToastViewport>
+>(({ className, ...props }, forwardedRef) => {
+	return (
+		<ToastPrimitives.ToastViewport
+			{...props}
+			ref={forwardedRef}
+			className={cn(
+				// 'pointer-events-none fixed inset-0 flex items-end justify-center p-4',
+				'',
+				className,
+			)}
+		/>
+	)
+})
+
+ToastViewport.displayName = ToastPrimitives.ToastViewport.displayName
 
 export function Toast() {
 	return (
