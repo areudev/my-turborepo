987d09b297007942214352bd0102181cedbd6d39
Chris Lime <chrkalivas@gmail.com>
2024-02-19 14:25:37 +0200

    Add hover effect and card details to fluid cards component

--- apps/framer-recipes/components/fluid-cards.tsx
+++ apps/framer-recipes/components/fluid-cards.tsx
@@ -5,13 +5,24 @@ export function FluitCards() {
 				{imageIds.map(item => (
 					<li
 						key={item}
-						className="relative h-[500px] w-full overflow-hidden rounded-2xl bg-rose-300"
+						className="group relative h-[500px] w-full overflow-hidden rounded-2xl bg-rose-300"
 					>
 						<img
 							src={`https://images.unsplash.com/photo-${item}?w=800&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8Mnx8dmlicmFudHxlbnwwfHwwfHx8MA%3D%3D`}
 							alt=""
-							className="absolute inset-0 h-full w-full  object-cover "
+							className="absolute inset-0 h-full w-full object-cover"
 						/>
+						<div className="absolute inset-x-0 bottom-0 bg-gradient-to-t from-black/80 from-30%  p-4">
+							<h2 className="text-2xl font-medium text-white">
+								The card title is here.
+							</h2>
+							<div className="grid grid-rows-[0fr] transition-all group-hover:grid-rows-[1fr]">
+								<p className="mt-2 overflow-hidden text-white/70 transition-all ">
+									Lorem ipsum dolor sit, amet consectetur adipisicing elit.
+									Minima quia ipsa eius.
+								</p>
+							</div>
+						</div>
 					</li>
 				))}
 			</ul>
