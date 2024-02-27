17d8cc1ac2aad2f65ff45c75b971471a9dcaf855
Chris Lime <chrkalivas@gmail.com>
2024-02-28 00:56:55 +0200

    Update framer-motion imports and add scrollYBoundedProgress

--- apps/framer-recipes/components/fixed-header.tsx
+++ apps/framer-recipes/components/fixed-header.tsx
@@ -1,6 +1,12 @@
 'use client'
 
-import { useMotionValue, useScroll, motion, useTransform } from 'framer-motion'
+import {
+	useMotionValue,
+	useScroll,
+	motion,
+	useTransform,
+	useMotionTemplate,
+} from 'framer-motion'
 import { useEffect } from 'react'
 
 const clamp = (value: number, min: number, max: number) =>
@@ -9,6 +15,11 @@ const clamp = (value: number, min: number, max: number) =>
 function useBoundedScroll(bounds: number) {
 	const { scrollY } = useScroll()
 	const scrollYBounded = useMotionValue(0)
+	const scrollYBoundedProgress = useTransform(
+		scrollYBounded,
+		[0, bounds],
+		[0, 1],
+	)
 	useEffect(() => {
 		return scrollY.on('change', current => {
 			const previous = scrollY.getPrevious()
@@ -17,41 +28,22 @@ function useBoundedScroll(bounds: number) {
 
 			scrollYBounded.set(clamp(newScrollYBounded, 0, bounds))
 		})
-	}, [])
+	}, [bounds, scrollY, scrollYBounded])
 
-	return { scrollYBounded }
+	return { scrollYBounded, scrollYBoundedProgress }
 }
 
 export function FixedHeader() {
-	const bounds = 50
-	const { scrollYBounded } = useBoundedScroll(bounds)
-	const height = useTransform(scrollYBounded, [0, bounds], [80, 50])
-	const opacity = useTransform(scrollYBounded, [0, bounds], [1, 0])
-
-	useEffect(() => {
-		return scrollYBounded.on('change', v => {
-			console.log(v)
-		})
-	}, [scrollYBounded])
-
-	// const { scrollY } = useScroll()
-	// // const height = useTransform(scrollY, v => Math.max(80 - 0.1 * v, 50))
-	// const height = useMotionValue(80)
-	// const opacity = useMotionValue(1)
-
-	// useEffect(() => {
-	// 	return scrollY.on('change', current => {
-	// 		const previous = scrollY.getPrevious()
-	// 		const diff = current - (previous ?? current)
-	// 		const newHeight = height.get() - diff
-	// 		const newOpacity = opacity.get() - diff * 0.1
-
-	// 		height.set(clamp(newHeight, 50, 80))
-	// 		opacity.set(clamp(newOpacity, 0, 1))
-	// 		console.log(diff)
-	// 	})
-	// }, [scrollY, height])
-	// console.log(scrollY.get())
+	const bounds = 500
+	const { scrollYBoundedProgress } = useBoundedScroll(bounds)
+	const scrollYBoundedProgressThrottled = useTransform(
+		scrollYBoundedProgress,
+		[0, 0.7, 1],
+		[0, 0, 1],
+	)
+	const height = useTransform(scrollYBoundedProgressThrottled, [0, 1], [80, 50])
+	const opacity = useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0])
+	const scale = useTransform(scrollYBoundedProgressThrottled, [0, 1], [1, 0.9])
 
 	return (
 		<div className="mx-auto flex w-full max-w-3xl flex-1 overflow-hidden text-slate-600">
@@ -59,18 +51,28 @@ export function FixedHeader() {
 				<motion.header
 					style={{
 						height,
+						backgroundColor: useMotionTemplate`rgb(255 255 255 / ${useTransform(
+							scrollYBoundedProgressThrottled,
+							[0, 1],
+							[1, 0.1],
+						)})`,
 					}}
-					className="fixed inset-x-0 flex h-20 bg-white shadow"
+					className="fixed inset-x-0 flex h-20  shadow backdrop-blur-md"
 				>
 					<div className="mx-auto flex w-full max-w-3xl items-center justify-between px-8">
-						<p className="flex origin-left items-center text-xl font-semibold uppercase">
+						<motion.p
+							style={{
+								scale,
+							}}
+							className="flex origin-left items-center text-xl font-semibold uppercase"
+						>
 							<span className="-ml-1.5 inline-block -rotate-90 text-[10px] leading-[0]">
 								The
 							</span>
 							<span className="-ml-1 text-2xl tracking-[-.075em]">
 								Daily Bugle
 							</span>
-						</p>
+						</motion.p>
 						<motion.nav
 							style={{
 								opacity: opacity,
