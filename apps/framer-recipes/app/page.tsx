0883945647f8869f4bbdbc2d38141de1dd0a2b5a
Chris Lime <chrkalivas@gmail.com>
2024-02-27 18:11:29 +0200

    Add FixedHeader component

--- apps/framer-recipes/app/page.tsx
+++ apps/framer-recipes/app/page.tsx
@@ -1,7 +1,8 @@
 // import { Steps } from '@/components/steps'
 // import { FluitCards } from '@/components/fluid-cards'
 import Email from '@/components/email'
+import { FixedHeader } from '@/components/fixed-header'
 
 export default function Home() {
-	return <Email />
+	return <FixedHeader />
 }
