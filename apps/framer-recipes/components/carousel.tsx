ef48605892d04c2241745f4a30d094410dcc33ff
Chris Lime <chrkalivas@gmail.com>
2024-02-29 00:12:27 +0200

    Update carousel icons and transitions

--- apps/framer-recipes/components/carousel.tsx
+++ apps/framer-recipes/components/carousel.tsx
@@ -1,8 +1,8 @@
 'use client'
 
 import { useState } from 'react'
-import { motion } from 'framer-motion'
-import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
+import { AnimatePresence, MotionConfig, motion } from 'framer-motion'
+import { CaretLeftIcon, CaretRightIcon } from '@radix-ui/react-icons'
 
 const images = [
 	'/1.jpeg',
@@ -14,51 +14,57 @@ const images = [
 ]
 
 export function Carousel() {
-	const [index, setIndex] = useState(0)
+	const [index, setIndex] = useState(1)
 
 	return (
-		<div className="h-full bg-black">
-			<div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
-				<div className="relative">
-					<div className="relative overflow-hidden">
-						<motion.div
-							animate={{ x: `-${index * 100}%` }}
-							transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}
-							className="flex"
-						>
-							{images.map(image => (
-								<img
-									key={image}
-									src={image}
-									className="aspect-[3/2] object-cover"
-								/>
-							))}
-						</motion.div>
-					</div>
-					{/* <img
-						src={images[index]}
-						alt="carousel"
-						className="aspect-[3/2] object-cover"
-					/> */}
-					{index > 0 && (
-						<button
-							className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
-							onClick={() => setIndex(index - 1)}
-						>
-							<ArrowLeftIcon className="h-6 w-6" />
-						</button>
-					)}
+		<MotionConfig transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}>
+			<div className="h-full bg-black">
+				<div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
+					<div className="relative">
+						<div className="relative overflow-hidden">
+							<motion.div animate={{ x: `-${index * 100}%` }} className="flex">
+								{images.map(image => (
+									<img
+										key={image}
+										src={image}
+										className="aspect-[3/2] object-cover"
+									/>
+								))}
+							</motion.div>
+						</div>
 
-					{index + 1 < images.length && (
-						<button
-							className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
-							onClick={() => setIndex(index + 1)}
-						>
-							<ArrowRightIcon className="h-6 w-6" />
-						</button>
-					)}
+						<AnimatePresence>
+							{index > 0 && (
+								<motion.button
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 0.7 }}
+									exit={{ opacity: 0, pointerEvents: 'none' }}
+									whileHover={{ scale: 1.1, opacity: 1 }}
+									className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+									onClick={() => setIndex(index - 1)}
+								>
+									<CaretLeftIcon className="h-8 w-8" />
+								</motion.button>
+							)}
+						</AnimatePresence>
+
+						<AnimatePresence>
+							{index + 1 < images.length && (
+								<motion.button
+									initial={{ opacity: 0 }}
+									animate={{ opacity: 0.7 }}
+									exit={{ opacity: 0, pointerEvents: 'none' }}
+									whileHover={{ scale: 1.1, opacity: 1 }}
+									className="absolute right-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white"
+									onClick={() => setIndex(index + 1)}
+								>
+									<CaretRightIcon className="h-6 w-6" />
+								</motion.button>
+							)}
+						</AnimatePresence>
+					</div>
 				</div>
 			</div>
-		</div>
+		</MotionConfig>
 	)
 }
