6ba779e20b2ead1786a8fe6f3f9a314e0de66b5e
Chris Lime <chrkalivas@gmail.com>
2024-02-29 00:00:22 +0200

    Refactor carousel component to use framer-motion for animation

--- apps/framer-recipes/components/carousel.tsx
+++ apps/framer-recipes/components/carousel.tsx
@@ -1,8 +1,8 @@
 'use client'
 
-import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
-
 import { useState } from 'react'
+import { motion } from 'framer-motion'
+import { ArrowLeftIcon, ArrowRightIcon } from '@radix-ui/react-icons'
 
 const images = [
 	'/1.jpeg',
@@ -14,17 +14,32 @@ const images = [
 ]
 
 export function Carousel() {
-	const [index, setIndex] = useState(2)
+	const [index, setIndex] = useState(0)
 
 	return (
 		<div className="h-full bg-black">
 			<div className="mx-auto flex h-full max-w-7xl flex-col justify-center">
 				<div className="relative">
-					<img
+					<div className="relative overflow-hidden">
+						<motion.div
+							animate={{ x: `-${index * 100}%` }}
+							transition={{ duration: 0.7, ease: [0.32, 0.72, 0, 1] }}
+							className="flex"
+						>
+							{images.map(image => (
+								<img
+									key={image}
+									src={image}
+									className="aspect-[3/2] object-cover"
+								/>
+							))}
+						</motion.div>
+					</div>
+					{/* <img
 						src={images[index]}
 						alt="carousel"
 						className="aspect-[3/2] object-cover"
-					/>
+					/> */}
 					{index > 0 && (
 						<button
 							className="absolute left-2 top-1/2 -mt-4 flex h-8 w-8 items-center justify-center rounded-full bg-white/60 transition hover:bg-white/80"
