ff2445238980c72997f2b56aad8ce0acd4552738
Chris Lime <chrkalivas@gmail.com>
2024-03-03 23:24:28 +0200

    Refactor ResisablePanel component and add motion animations

--- apps/framer-recipes/components/resisable-panel.tsx
+++ apps/framer-recipes/components/resisable-panel.tsx
@@ -6,57 +6,77 @@ import { CheckIcon } from '@radix-ui/react-icons'
 import { createContext } from 'react'
 import { useContext } from 'react'
 
-let transition = { type: 'ease', ease: 'easeInOut', duration: 1 }
+const transition = { type: 'ease', ease: 'easeInOut', duration: 1 }
+
+type Status = 'idle' | 'saving' | 'success'
 
 export function ResisablePanel() {
-	let [status, setStatus] = useState('idle')
+	const [status, setStatus] = useState('idle')
 
 	return (
-		<div className="flex min-h-screen flex-col items-start bg-zinc-900 pt-28">
-			<div className="mx-auto w-full max-w-md">
-				<div className="rounded-2xl border border-zinc-700 bg-zinc-800">
-					<div className="px-8 pt-8">
-						<p className="text-lg text-white">Reset password</p>
+		<MotionConfig transition={{ duration: 1 }}>
+			<div className="flex min-h-screen flex-col items-start bg-zinc-900 pt-28">
+				<div className="mx-auto w-full max-w-md">
+					<div className="rounded-2xl border border-zinc-700 bg-zinc-800">
+						<div className="px-8 pt-8">
+							<p className="text-lg text-white">Reset password</p>
+						</div>
+
+						<AnimatePresence mode="wait">
+							{status === 'idle' || status === 'saving' ? (
+								<motion.div
+									exit={{
+										opacity: 0,
+									}}
+									key="form"
+								>
+									<Form
+										onSubmit={async () => await delay(1000)}
+										afterSave={() => setStatus('success')}
+										className="p-8"
+									>
+										<p className="text-sm text-zinc-400">
+											Enter your email to get a password reset link:
+										</p>
+
+										<div className="mt-3">
+											<input
+												className="block w-full rounded border-none p-2 text-slate-900"
+												type="email"
+												required
+												defaultValue="sam@buildui.com"
+											/>
+										</div>
+										<div className="mt-8 text-right">
+											<Form.Button className="rounded bg-indigo-500 px-5 py-2 text-sm font-medium text-white ">
+												Email me my link
+											</Form.Button>
+										</div>
+									</Form>
+								</motion.div>
+							) : (
+								<motion.div initial={{ opacity: 0 }} animate={{ opacity: 1 }}>
+									<p className="p-8 text-sm text-zinc-400">
+										Email sent! Check your inbox to continue.
+									</p>
+								</motion.div>
+							)}
+						</AnimatePresence>
 					</div>
 
-					{status === 'idle' || status === 'saving' ? (
-						<div>
-							<Form
-								onSubmit={async () => await delay(1000)}
-								afterSave={() => setStatus('success')}
-								className="p-8"
-							>
-								<p className="text-sm text-zinc-400">
-									Enter your email to get a password reset link:
-								</p>
-								<div className="mt-3">
-									<input
-										className="block w-full rounded border-none p-2 text-slate-900"
-										type="email"
-										required
-										defaultValue="sam@buildui.com"
-									/>
-								</div>
-								<div className="mt-8 text-right">
-									<Form.Button className="rounded bg-indigo-500 px-5 py-2 text-sm font-medium text-white ">
-										Email me my link
-									</Form.Button>
-								</div>
-							</Form>
-						</div>
-					) : (
-						<p className="p-8 text-sm text-zinc-400">
-							Email sent! Check your inbox to continue.
-						</p>
-					)}
+					<p className="mt-8 text-sm text-zinc-500">
+						<span className="underline">Reach out</span> to us if you need more
+						help.
+					</p>
 				</div>
-
-				<p className="mt-8 text-sm text-zinc-500">
-					<span className="underline">Reach out</span> to us if you need more
-					help.
-				</p>
+				<button
+					onClick={() => setStatus('idle')}
+					className="mx-auto mt-8 rounded bg-indigo-500 px-4 py-2 text-white"
+				>
+					do it again
+				</button>
 			</div>
-		</div>
+		</MotionConfig>
 	)
 }
 
@@ -136,7 +156,7 @@ Form.Button = function FormButton({
 							key="b"
 							initial={{ opacity: 0 }}
 							animate={{ opacity: 1 }}
-							className="absolute inset-0 flex justify-center py-2"
+							className="absolute inset-0 flex justify-center py-2 "
 						>
 							<CheckIcon className="h-full" />
 						</motion.div>
