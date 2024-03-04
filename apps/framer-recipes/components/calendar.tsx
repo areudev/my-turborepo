ba1dd6d0f083a44848e3057b8eb8d3bf2ce13b9b
Chris Lime <chrkalivas@gmail.com>
2024-03-04 23:21:36 +0200

    Add motion animation to calendar component

--- apps/framer-recipes/components/calendar.tsx
+++ apps/framer-recipes/components/calendar.tsx
@@ -14,6 +14,7 @@ import {
 	subMonths,
 } from 'date-fns'
 import { useState } from 'react'
+import { AnimatePresence, MotionConfig, motion } from 'framer-motion'
 
 export default function Calendar() {
 	const [monthString, setMonthString] = useState(format(new Date(), 'yyyy-MM'))
@@ -36,50 +37,60 @@ export default function Calendar() {
 	})
 
 	return (
-		<div className="flex min-h-screen items-start bg-stone-800 pt-16 text-stone-900">
-			<div className="mx-auto w-full max-w-md rounded-2xl bg-white">
-				<div className="py-8">
-					<div className="flex flex-col justify-center rounded text-center">
-						<header className="relative flex justify-between px-8">
-							<button
-								className="z-10 rounded-full p-1.5 hover:bg-stone-100"
-								onClick={previousMonth}
-							>
-								<ChevronLeftIcon className="h-4 w-4" />
-							</button>
-							<p className="absolute inset-0 flex items-center justify-center font-semibold">
-								{format(month, 'MMMM yyyy')}
-							</p>
-							<button
-								className="z-10 rounded-full p-1.5 hover:bg-stone-100"
-								onClick={nextMonth}
-							>
-								<ChevronRightIcon className="h-4 w-4" />
-							</button>
-						</header>
-						<div className="mt-6 grid grid-cols-7 gap-y-6 px-8">
-							<span className="font-medium text-stone-500">Su</span>
-							<span className="font-medium text-stone-500">Mo</span>
-							<span className="font-medium text-stone-500">Tu</span>
-							<span className="font-medium text-stone-500">We</span>
-							<span className="font-medium text-stone-500">Th</span>
-							<span className="font-medium text-stone-500">Fr</span>
-							<span className="font-medium text-stone-500">Sa</span>
-
-							{days.map(day => (
-								<span
-									className={`${
-										isSameMonth(day, month) ? '' : 'text-stone-300'
-									} font-semibold `}
-									key={format(day, 'yyyy-MM-dd')}
+		<MotionConfig transition={{ duration: 3 }}>
+			<div className="flex min-h-screen items-start bg-stone-800 pt-16 text-stone-900">
+				<div className="relative mx-auto w-full max-w-md overflow-hidden rounded-2xl bg-white">
+					<div className="py-8">
+						<div className="flex flex-col justify-center rounded text-center">
+							<AnimatePresence mode="popLayout" initial={false}>
+								<motion.div
+									key={monthString}
+									initial={{ x: '100%' }}
+									animate={{ x: '0%' }}
+									exit={{ x: '-100%' }}
 								>
-									{format(day, 'd')}
-								</span>
-							))}
+									<header className="relative flex justify-between px-8">
+										<button
+											className="z-10 rounded-full p-1.5 hover:bg-stone-100"
+											onClick={previousMonth}
+										>
+											<ChevronLeftIcon className="h-4 w-4" />
+										</button>
+										<p className="absolute inset-0 flex items-center justify-center font-semibold">
+											{format(month, 'MMMM yyyy')}
+										</p>
+										<button
+											className="z-10 rounded-full p-1.5 hover:bg-stone-100"
+											onClick={nextMonth}
+										>
+											<ChevronRightIcon className="h-4 w-4" />
+										</button>
+									</header>
+									<div className="mt-6 grid grid-cols-7 gap-y-6 px-8">
+										<span className="font-medium text-stone-500">Su</span>
+										<span className="font-medium text-stone-500">Mo</span>
+										<span className="font-medium text-stone-500">Tu</span>
+										<span className="font-medium text-stone-500">We</span>
+										<span className="font-medium text-stone-500">Th</span>
+										<span className="font-medium text-stone-500">Fr</span>
+										<span className="font-medium text-stone-500">Sa</span>
+										{days.map(day => (
+											<span
+												className={`${
+													isSameMonth(day, month) ? '' : 'text-stone-300'
+												} font-semibold `}
+												key={format(day, 'yyyy-MM-dd')}
+											>
+												{format(day, 'd')}
+											</span>
+										))}
+									</div>
+								</motion.div>
+							</AnimatePresence>
 						</div>
 					</div>
 				</div>
 			</div>
-		</div>
+		</MotionConfig>
 	)
 }
