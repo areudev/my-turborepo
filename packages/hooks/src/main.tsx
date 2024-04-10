5103d4b57e4a706fe8f25d65b1a0e46b9537323d
Christos Kalivas <92673248+areudev@users.noreply.github.com>
2024-04-10 16:19:49 +0300

    Refactor App component and add useDocumentTitle hook

--- packages/hooks/src/main.tsx
+++ packages/hooks/src/main.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
 import './index.css'
+import DocumentTitle from './examples/document-title.tsx'
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 ReactDOM.createRoot(document.getElementById('root')!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+	<React.StrictMode>
+		<DocumentTitle />
+	</React.StrictMode>,
 )
