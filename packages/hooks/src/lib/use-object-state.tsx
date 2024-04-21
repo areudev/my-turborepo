4e8ef98eb159ef4f21491641127d7c0278be2fe8
Christos Kalivas <92673248+areudev@users.noreply.github.com>
2024-04-21 13:33:14 +0300

    Refactor useObjectState hook to use useCallback in hooks package

--- packages/hooks/src/lib/use-object-state.tsx
+++ packages/hooks/src/lib/use-object-state.tsx
@@ -1,9 +1,4 @@
-// function isPlainObject(
-// 	value: unknown,
-// ): value is Record<string | number | symbol, unknown> {
-// 	return Object.prototype.toString.call(value) === '[object Object]'
-
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 
 function isPlainObject(
 	value: unknown,
@@ -19,19 +14,22 @@ function isPlainObject(
 export function useObjectState<T extends Record<string, unknown>>(initial: T) {
 	const [state, setState] = useState(initial)
 
-	const updateState = (newState: Partial<T> | ((state: T) => Partial<T>)) => {
-		setState(prevState => {
-			let nextState: Partial<T>
-			if (typeof newState === 'function') {
-				nextState = newState(prevState)
-			} else {
-				nextState = newState
-			}
-			nextState = isPlainObject(nextState) ? nextState : {}
+	const updateState = useCallback(
+		(newState: Partial<T> | ((state: T) => Partial<T>)) => {
+			setState(prevState => {
+				let nextState: Partial<T>
+				if (typeof newState === 'function') {
+					nextState = newState(prevState)
+				} else {
+					nextState = newState
+				}
+				nextState = isPlainObject(nextState) ? nextState : {}
 
-			return { ...prevState, ...nextState }
-		})
-	}
+				return { ...prevState, ...nextState }
+			})
+		},
+		[],
+	)
 
 	return [state, updateState] as const
 }
