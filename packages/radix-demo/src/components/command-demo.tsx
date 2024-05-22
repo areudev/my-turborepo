6218992ba6f8fe49e291b93eddbb1cebb0ae3ca7
Christos Kalivas <92673248+areudev@users.noreply.github.com>
2024-05-22 20:31:32 +0300

    feat: Add @radix-ui/react-popover dependency and update Command component to use CommandPopover

--- packages/radix-demo/src/components/command-demo.tsx
+++ packages/radix-demo/src/components/command-demo.tsx
@@ -1,62 +1,62 @@
 import React from 'react'
 import {
 	Command,
-	CommandListDialoged,
+	CommandPopover,
+	CommandList,
 	CommandEmpty,
 	CommandGroup,
 	CommandInput,
 	CommandItem,
 	CommandSeparator,
 	CommandShortcut,
-} from '../lib/command'
+} from '../lib/command-popped'
 import { SpeakerXMark } from './icons'
 
 export function CommandDemo() {
 	const [open, setOpen] = React.useState(false)
 	return (
 		<div className="w-80">
-			<Command className="rounded-lg border shadow-md">
-				<CommandInput placeholder="Type a command or search..." />
-				<CommandListDialoged
-					// modal={false}
-					modal={false}
-					open={open}
-				>
-					<CommandEmpty>No results found.</CommandEmpty>
-					<CommandGroup heading="Suggestions">
-						<CommandItem>
-							<SpeakerXMark className="mr-2 h-4 w-4" />
-							<span>Calendar</span>
-						</CommandItem>
-						<CommandItem>
-							<SpeakerXMark className="mr-2 h-4 w-4" />
-							<span>Search Emoji</span>
-						</CommandItem>
-						<CommandItem>
-							<SpeakerXMark className="mr-2 h-4 w-4" />
-							<span>Launch</span>
-						</CommandItem>
-					</CommandGroup>
-					<CommandSeparator />
-					<CommandGroup heading="Settings">
-						<CommandItem>
-							<SpeakerXMark className="mr-2 h-4 w-4" />
-							<span>Profile</span>
-							<CommandShortcut>⌘P</CommandShortcut>
-						</CommandItem>
-						<CommandItem>
-							<SpeakerXMark className="mr-2 h-4 w-4" />
-							<span>Mail</span>
-							<CommandShortcut>⌘B</CommandShortcut>
-						</CommandItem>
-						<CommandItem>
-							<SpeakerXMark className="mr-2 h-4 w-4" />
-							<span>Settings</span>
-							<CommandShortcut>⌘S</CommandShortcut>
-						</CommandItem>
-					</CommandGroup>
-				</CommandListDialoged>
-			</Command>
+			<CommandPopover open={open} onOpenChange={setOpen}>
+				<Command className="rounded-lg border shadow-md">
+					<CommandInput placeholder="Type a command or search..." />
+					<CommandList>
+						<CommandEmpty>No results found.</CommandEmpty>
+						<CommandGroup heading="Suggestions">
+							<CommandItem>
+								<SpeakerXMark className="mr-2 h-4 w-4" />
+								<span>Calendar</span>
+							</CommandItem>
+							<CommandItem>
+								<SpeakerXMark className="mr-2 h-4 w-4" />
+								<span>Search Emoji</span>
+							</CommandItem>
+							<CommandItem>
+								<SpeakerXMark className="mr-2 h-4 w-4" />
+								<span>Launch</span>
+							</CommandItem>
+						</CommandGroup>
+						<CommandSeparator />
+						<CommandGroup heading="Settings">
+							<CommandItem>
+								<SpeakerXMark className="mr-2 h-4 w-4" />
+								<span>Profile</span>
+								<CommandShortcut>⌘P</CommandShortcut>
+							</CommandItem>
+							<CommandItem>
+								<SpeakerXMark className="mr-2 h-4 w-4" />
+								<span>Mail</span>
+								<CommandShortcut>⌘B</CommandShortcut>
+							</CommandItem>
+							<CommandItem>
+								<SpeakerXMark className="mr-2 h-4 w-4" />
+								<span>Settings</span>
+								<CommandShortcut>⌘S</CommandShortcut>
+							</CommandItem>
+						</CommandGroup>
+					</CommandList>
+				</Command>
+			</CommandPopover>
+
 			<button
 				onClick={() => {
 					setOpen(!open)
