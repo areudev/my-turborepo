b438fa0d03172693a2a7a54a42879ed94810c608
Christos Kalivas <92673248+areudev@users.noreply.github.com>
2024-05-23 12:27:05 +0300

    gdfsgfdgdg

--- packages/radix-demo/src/components/command-demo.tsx
+++ packages/radix-demo/src/components/command-demo.tsx
@@ -1,50 +1,67 @@
 import { useEffect, useRef, useState } from 'react'
 import {
 	Command,
-	CommandDialog,
+	// CommandDialog,
 	CommandEmpty,
 	CommandGroup,
 	CommandInput,
 	CommandItem,
 	CommandList,
-	CommandListDialoged,
+	// CommandListDialoged,
 } from '../lib/command-dialoged'
-import { useClickAway } from '../hooks/click-outside'
+import { useEventListener } from '../hooks/event'
 
 export function CommandDemo() {
 	const [open, setOpen] = useState(false)
-	const ref = useClickAway(() => {
+	const ref = useRef<HTMLDivElement | null>(null)
+
+	const handleClick = (e: MouseEvent | TouchEvent) => {
+		const element = ref.current
+		if (!element || element.contains(e.target as Node)) return
 		if (open) {
 			setOpen(false)
 		}
-	})
+	}
+	useEventListener('mousedown', handleClick)
+	useEventListener('touchstart', handleClick)
+
 	return (
-		<div className="flex items-center justify-center">
-			<Command>
+		<div className="flex w-96 items-center justify-center">
+			<Command ref={ref}>
 				<CommandInput
 					onClick={() => {
-						setOpen(open => !open)
+						if (!open) setOpen(open => !open)
 					}}
 					placeholder="Type a command or search..."
 				/>
 				{open ? (
-					<CommandList ref={ref}>
+					<CommandList>
 						<CommandEmpty>No results found.</CommandEmpty>
-						<CommandGroup heading="Suggestions">
-							<CommandItem>Calendar</CommandItem>
-							<CommandItem>Search Emoji</CommandItem>
-							<CommandItem>Calculator</CommandItem>
+						<CommandGroup heading="books">
+							{books.map(book => (
+								<CommandItem key={book.id}>{book.title}</CommandItem>
+							))}
 						</CommandGroup>
 					</CommandList>
 				) : null}
 			</Command>
-			<button
-				onClick={() => {
-					setOpen(open => !open)
-				}}
-			>
-				Click me
-			</button>
 		</div>
 	)
 }
+
+const books = [
+	{ id: 'book-1', author: 'Harper Lee', title: 'To Kill a Mockingbird' },
+	{ id: 'book-2', author: 'Lev Tolstoy', title: 'War and Peace' },
+	{ id: 'book-3', author: 'Fyodor Dostoyevsy', title: 'The Idiot' },
+	{ id: 'book-4', author: 'Oscar Wilde', title: 'A Picture of Dorian Gray' },
+	{ id: 'book-5', author: 'George Orwell', title: '1984' },
+	{ id: 'book-6', author: 'Jane Austen', title: 'Pride and Prejudice' },
+	{ id: 'book-7', author: 'Marcus Aurelius', title: 'Meditations' },
+	{
+		id: 'book-8',
+		author: 'Fyodor Dostoevsky',
+		title: 'The Brothers Karamazov',
+	},
+	{ id: 'book-9', author: 'Lev Tolstoy', title: 'Anna Karenina' },
+	{ id: 'book-10', author: 'Fyodor Dostoevsky', title: 'Crime and Punishment' },
+]
